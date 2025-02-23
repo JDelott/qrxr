f1cefe5d40373b455bba08ec4dc1846db592d6ea
jdelott <jldelott@gmail.com>
2025-02-23 17:20:54 +0000

    Restored state from snapshot

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // This is where the built files will go
+    },
+    server: {
+        host: true,
+        port: 5174,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            },
+        },
+    },
+});
